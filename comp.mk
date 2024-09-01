6286df27dc84ccc631e5cd6d93596addb326a484
221V <221v92@gmail.com>
2024-09-01 06:54:59 +0300

    add D

--- comp.mk
+++ comp.mk
@@ -4,6 +4,7 @@
 clean:
 	rm program_c
 	rm program_nim
+	rm program_d
 
 
 # clean C compiled
@@ -16,8 +17,13 @@ clean2:
 	rm program_nim
 
 
+# clean D compiled
+clean3:
+	rm program_d
+
+
 # compile all
-all: c1 c2
+all: c1 c2 c3
 
 
 # compile C program
@@ -33,6 +39,13 @@ c2:
 ## https://nim-lang.org/docs/nimc.html
 
 
+# compile D program
+c3:
+	ldc2 -mtriple=x86_64-linux-gnu -O -static -of=program_d program.d
+#	ldc2 -mtriple=x86_64-linux-gnu -O --betterC -of=program_d program.d
+## https://wiki.dlang.org/LDC
+
+
 # run C program
 run1:
 	./program_c
@@ -43,11 +56,26 @@ run2:
 	./program_nim
 
 
+# run D program
+run3:
+	./program_d
+
+
 # run poop benchmark C and Nim programs
 poop12:
 	poop ./program_c ./program_nim
 
 
+# run poop benchmark C and D programs
+poop13:
+	poop ./program_c ./program_d
+
+
+# run poop benchmark Nim and D programs
+poop23:
+	poop ./program_nim ./program_d
+
+
 default: all
-.PHONY: clean clean1 clean2 all c1 c2 run1 run2 poop12
+.PHONY: clean clean1 clean2 clean3 all c1 c2 c3 run1 run2 run3 poop12 poop13 poop23
 
