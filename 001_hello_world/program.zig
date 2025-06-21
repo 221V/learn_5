de0c7c40db7cd6486a19722f2d12698daa7a4284
221V <221v92@gmail.com>
2025-06-21 03:22:40 +0300

    001 zig - just string

--- 001_hello_world/program.zig
+++ 001_hello_world/program.zig
@@ -4,6 +4,7 @@ const print = std.debug.print;
 
 pub fn main() void{
   //std.debug.print("Hello, {s}!\n", .{"World"});
+  std.debug.print("Hello, World!\n", .{});
   print("Hello, {s}!\n", .{"World"});
 }
 
