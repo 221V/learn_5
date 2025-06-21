b69c42e541d1a5f0d397fe4572983ca89a2e2e05
221V <221v92@gmail.com>
2025-06-21 03:20:44 +0300

    001 zig const as alias

--- 001_hello_world/program.zig
+++ 001_hello_world/program.zig
@@ -1,7 +1,9 @@
 
 const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() void{
-  std.debug.print("Hello, {s}!\n", .{"World"});
+  //std.debug.print("Hello, {s}!\n", .{"World"});
+  print("Hello, {s}!\n", .{"World"});
 }
 
